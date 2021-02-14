c215612332c322a59223fbc45499de4ccc868d04
Nick Østergaard <oe.nick@gmail.com>
2021-02-14 01:06:53 +0100

    Test code for initing the FPGA
    CLK_OUT seems to work fine no, aka the "hacked" differential clock works
    too

--- litex_boards/targets/test_dac.py
+++ litex_boards/targets/test_dac.py
@@ -25,13 +25,29 @@ wb.regs.ctrl_reset.write(1)
 for name, reg in wb.regs.__dict__.items():
     print("0x{:08x} : 0x{:08x} {}".format(reg.addr, reg.read(), name))
 
-time.sleep(1)
 print("NICK DEBUG")
+time.sleep(2)
+#The control word is latched
+#on the rising edge of CW. CW is independent of the DAC
+#clock. The DAC clock can always remain running, when
+#the control word is written to the DAC.
+wb.write(wb.regs.dac_cw.addr, 1)
+wb.write(wb.regs.dac_cw.addr, 0)
+
+#writhing the same data to both registers to be "safe"
+wb.write(wb.regs.dac_data_a_storage.addr, 0x120)
+wb.write(wb.regs.dac_data_b_storage.addr, 0x120)
+
+#latch in the settings.
+wb.write(wb.regs.dac_cw.addr, 1)
+
+
+#now we should be able to stream to the DAC
+
 for i in range(1, 42):
     print(f"Writing dac_data_a_storage {i}")
     wb.write(wb.regs.dac_data_a_storage.addr, i)
-
-    time.sleep(1)
+    time.sleep(0.1)
 
 
 wb.close()
